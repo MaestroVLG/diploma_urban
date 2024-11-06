d7447737239a8851c85d7a5ace1539bb5cdaa5e7
MaestroVLG <las-vegas1945@yandex.ru>
2024-11-07 02:33:15 +0300

    /

---
+++ app.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
+
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretnyi_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        products = Product.query.filter_by(user_id=current_user.id).all()
+    else:
+        products = []
+    return render_template('index.html', products=products)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'], method='pbkdf2:sha256')
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Регистрация прошла успешно!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Ошибка авторизации! Проверьте логин и пароль.', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/add_product', methods=['GET', 'POST'])
+@login_required
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        new_product = Product(name=name, price=price, user_id=current_user.id)
+        db.session.add(new_product)
+        db.session.commit()
+        flash('Продукт добавлен!', 'success')
+        return redirect(url_for('index'))
+    return render_template('add_product.html')
+
+@app.route('/edit_product/<int:product_id>', methods=['GET', 'POST'])
+@login_required
+def edit_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if product.user_id != current_user.id:
+        flash('Нет прав!!!', 'danger')
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.price = request.form['price']
+        db.session.commit()
+        flash('Продукт изменён', 'success')
+        return redirect(url_for('index'))
+    return render_template('edit_product.html', product=product)
+
+@app.route('/delete_product/<int:product_id>', methods=['POST'])
+@login_required
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if product.user_id != current_user.id:
+        flash('Вы не можете удалять продукт, у вас нет прав!', 'danger')
+        return redirect(url_for('index'))
+    db.session.delete(product)
+    db.session.commit()
+    flash('Продукт удалён!', 'success')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
